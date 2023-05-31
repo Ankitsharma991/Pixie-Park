3a3fb629808eba0f9fecb44514d6095492f81aa4
Ankit Sharma <sharmaa7861@gmail.com>
2023-05-31 15:45:49 +0530

    NavBar created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a href="/">
+          <img src="/images/home-icon.svg" alt="" />
+          <span>HOME</span>
+        </a>
+        <a href="/">
+          <img src="/images/search-icon.svg" alt="" />
+          <span>SEARCH</span>
+        </a>
+        <a href="/">
+          <img src="/images/watchlist-icon.svg" alt="" />
+          <span>WATCHLIST</span>
+        </a>
+        
+        <a href="/">
+          <img src="/images/original-icon.svg" alt="" />
+          <span>ORIGINALS</span>
+        </a>
+        <a href="/">
+          <img src="/images/movie-icon.svg" alt="" />
+          <span>MOVIES</span>
+        </a>
+        <a href="/">
+          <img src="/images/series-icon.svg" alt="" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+    </Nav>
+  );
 }
 
-export default Header
+export default Header;
+
+const Nav = styled.div`
+  height: 70px;
+  color: white;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    img {
+      height: 20px;
+    }
+  }
+  span {
+    color: white;
+    font-size: 13px;
+    letter-spacing: 1.42px;
+  }
+`;
