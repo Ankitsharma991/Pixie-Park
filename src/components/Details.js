61eb5556aebc2ff3527f85ee8c7271bb496d4ff3
Ankit Sharma <sharmaa7861@gmail.com>
2023-06-02 14:27:02 +0530

    Detail Page Added

--- src/components/Details.js
+++ src/components/Details.js
@@ -10,6 +10,35 @@ function Details() {
           alt=""
         />
       </Background>
+      <ImageTitle>
+        <img src="/images/bao-detail.png" alt="" />
+      </ImageTitle>
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" alt="" />
+          <span>Play</span>
+        </PlayButton>
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" alt="" />
+          <span>Trailer</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupWatchButton>
+          <img src="/images/group-icon.png" alt="" />
+        </GroupWatchButton>
+      </Controls>
+      <SubTitle>
+        2018 &#9675; 7m &#9675; Family, Fantasy, Kids, Animation
+      </SubTitle>
+      <Description>
+        A Chinese mom who's sad when her grown son is leaving home for the first
+        time finds solace in preparing a steaming hot bao, a traditional Chinese
+        dumpling, filled with love and memories. With each delicate fold and
+        pinch, she pours her affection into the dough, imbuing it with the
+        warmth and flavors of home.
+      </Description>
     </Container>
   );
 }
@@ -35,4 +64,78 @@ const Background = styled.div`
     width: 100%;
     object-fit: cover;
   }
+`;
+
+const ImageTitle = styled.div`
+  height: 30vh;
+  width: 35vw;
+  min-height: 170px;
+  margin-top: 80px; // margin
+  min-width: 200px;
+  margin-bottom: 50px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PlayButton = styled.button`
+  border-radius: 4px;
+  font-size: 15px;
+  padding: 8px 24px;
+  display: flex;
+  margin-right: 22px;
+  align-items: center;
+  height: 56px;
+  background: rgb(249, 249, 249);
+  border: none;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+`;
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+  text-transform: uppercase;
+`;
+const AddButton = styled.button`
+  margin-right: 16px;
+  width: 44px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`;
+const GroupWatchButton = styled(AddButton)`
+  background: rgb(0, 0, 0);
+`;
+
+const SubTitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 16px;
+`;
+
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 16px;
+  color: rgb(249, 249, 249);
 `;
